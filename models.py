43f3f4000c497181c0bca62cff91ce9abefe8cdb
Kris Vukasinovic <axellerate@hotmail.com>
2015-06-12 21:27:03 -0400

    fixed bug

--- models.py
+++ models.py
@@ -4,10 +4,9 @@ from google.appengine.ext import ndb
 This file contains the database models for WhatDo?
 '''
 
-
 class BaseModel(ndb.Model):
-	created = ndb.DateTimeProperty(auto_now = True)
-	updated = ndb.DateTimeProperty(auto_now_add = True)
+	created = ndb.DateTimeProperty(auto_now_add = True)
+	updated = ndb.DateTimeProperty(auto_now = True)
 
 class Users(BaseModel):
 	email = ndb.StringProperty(required = True)
