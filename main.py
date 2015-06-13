88d2ee29363f0fb0922d981f1f7a3ad4c3da1d0f
admin <axellerate@hotmail.com>
2015-06-13 14:57:06 -0400

    new update

--- main.py
+++ main.py
@@ -23,6 +23,13 @@ class PostResponse(messages.Message):
     message = messages.StringField(1)
     success = messages.BooleanField(2)
 
+# category view models
+class CategoryObject(messages.Message):
+    name = messages.StringField(1)
+
+class CategoryObjects(messages.Message):
+    categories = messages.MessageField(CategoryObject, 1, repeated = True)
+
 # user view models
 class UserObject(messages.Message):
     email = messages.StringField(1)
@@ -30,11 +37,17 @@ class UserObject(messages.Message):
     message = messages.StringField(3)
     success = messages.BooleanField(4)
 
-
-class QuestionObject(messages.Message):
-    
-
 # questions view models
+class QuestionObjectCreation(messages.Message):
+    title = messages.StringField(1)
+    flag_count = messages.IntegerField(2)
+    yes_count = messages.IntegerField(3)
+    no_count = messages.IntegerField(4)
+    visible = messages.BooleanField(5)
+    category_id = messages.IntegerField(6)
+    answered = messages.MessageField(CategoryObject, 7, repeated = True)
+    user_id = messages.IntegerField(8)
+
 class QuestionBatch(messages.Message):
     pass
 
@@ -47,13 +60,6 @@ class QuestionObject(messages.Message):
     message = messages.StringField(6)
     success = messages.BooleanField(7)
 
-
-# category view models
-class CategoryObject(messages.Message):
-    name = messages.StringField(1)
-
-class CategoryObjects(messages.Message):
-    categories = messages.MessageField(CategoryObject, 1, repeated = True)
 
 @endpoints.api(name = 'users', version = 'v1',
                description = 'User Management Resources')
@@ -88,17 +94,19 @@ class UsersApi(remote.Service):
                description = 'Questions Management Resources')
 class QuestionsApi(remote.Service):
 
-    @endpoints.method(CategoryObject, PostResponse,
+    @endpoints.method(QuestionObjectCreation, PostResponse,
                         name = 'create',
                         path = 'create',
                         http_method = 'POST')
-    def createCategory(self, request):
-        category_query = Categories.query(Categories.name == request.name)
-        if not category_query.get():
-            Categories(name = request.name).put()
-            post_response = PostResponse(message = "Post successful. Category created.", success = True)
-        else:
-            post_response = PostResponse(message = "Error. Category with that name already exists.", success = False)
+    def createQuestion(self, request):
+        user = Users.get_by_id(request.user_id)
+        category = Categories.get_by_id(request.category_id).key
+        question = Questions(title = request.title, category = category).put()
+        user.questions.append(question)
+        user.put()
+        post_response = PostResponse(message = "Post successful. Question created.", success = True)
+        # except:
+        #     post_response = PostResponse(message = "Error. Question was not created.", success = False)
         return post_response
 
 
