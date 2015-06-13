530cf0a66c02a66ad8752f05de5128d0965795fa
tpvretenarov <t394r@hotmail.com>
2015-06-13 12:32:04 -0400

    Models Update

--- models.py
+++ models.py
@@ -13,11 +13,40 @@ class Users(BaseModel):
 	password = ndb.StringProperty(required = True)
 	active = ndb.BooleanProperty(default = True)
 
-	# locked to administrators
-	def setUserAsInactive(self):
-		self.active = False
-		return
+class Questions(BaseModel):
+	title = ndb.StringProperty(required = True)
+	flag_count = ndb.IntegerProperty(default = 0)
+	yes_count = ndb.IntegerProperty(default = 0)
+	no_count = ndb.IntegerProperty(default = 0)
+	visible = ndb.BooleanProperty(default = True)
+	
+	def check_flag_count(self):
+		if (self.flag_count > 10):
+			self.visible = False
+	
+	@classmethod
+	def get_batch(self):
+		if (self.check_flag_count = True)
 
-	# locked to administrators
-	def setUserAsActive(self):
-		self.active = True
+
+class Categories(BaseModel):
+	name = ndb.StringProperty(required = True)
+
+class UsersToQuestions(BaseModel):
+	user = ndb.KeyProperty(default = True)
+	question = ndb.KeyProperty(default = True)
+
+class UsersToCategories(BaseModel):
+	user = ndb.KeyProperty(default = True)
+	category = ndb.KeyProperty(default = True)
+
+class QuestionsToCategories(BaseModel):
+	question = ndb.KeyProperty(default = True)
+	category = ndb.KeyProperty(default = True)
+
+class UsersFlaggedCount(BaseModel):
+	user = ndb.KeyProperty(default = True)
+	flag_count = ndb.IntegerProperty(default = 0)
+
+
+			
